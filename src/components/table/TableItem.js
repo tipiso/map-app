94236987886ac4730288713ad688b7f0827739c1
tipiso <tipiso@gmail.com>
2020-04-05 21:44:35 +0200

    markers coordinates added to the list

--- src/components/table/TableItem.js
+++ src/components/table/TableItem.js
@@ -5,7 +5,7 @@ function TableItem(props) {
 
     return (
         <div className="table-item table-item--container">
-            <p className="table-item">Marker ID: {props.id}</p>
+            <p className="table-item">Marker ID: {props.id} - Latitude: {props.values.lat} | Longitude: {props.values.lng}</p>
             <span className="table-item--delete" onClick={() => props.handleDelete(props.id)}>&#10006;</span>
         </div>
     );
